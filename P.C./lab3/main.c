910b035767032a5a406ccbb9b01a4b5a0f251632
Malacu <gpetre31@gmail.com>
2017-10-13 02:50:03 +0300

    ex1

---
+++ P.C./lab3/main.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+
+	int nr, i, sum=0;
+	
+	scanf("%d",&nr);
+	
+	for(i = 2 ; i < nr ; i++){
+		
+		if(nr % i == 0) {
+			printf("Numarul se divide cu %d\n",i);
+			
+			sum+=i;
+		
+		}
+		
+		
+	}
+
+		printf("Suma este %d \n",sum);
+
+	return 0;
+}
